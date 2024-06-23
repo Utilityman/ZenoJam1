4be09c5e556dc4484a35138fce31d0a77a67b05a
Josh Mackin <mackin711@gmail.com>
2024-06-23 14:54:10 -0400

    last changes

--- hazards/spawners/interval_projectile_spawner.gd
+++ hazards/spawners/interval_projectile_spawner.gd
@@ -2,7 +2,7 @@ class_name IntervalProjectileSpawner extends Node2D
 
 # TODO: this needs to export a whole sort of texture / animated thing (another packed scene??)
 # 	 	(the default icon.svg is unfortunately not good enough)
-@export_range(0.1, 15.0, 0.1, "or_greater") var interval: float = 0.0
+@export_range(0.0, 15.0, 0.1, "or_greater") var interval: float = 0.0
 
 # TODO: this almost needs to take a resource that builds the emit scene instead?
 @export var projectile_data: ProjectileHazardData
