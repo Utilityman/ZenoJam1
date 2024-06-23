024b13d4e0d9e364d595cf2a05329d0057352c8e
Josh Mackin <mackin711@gmail.com>
2024-06-23 14:23:56 -0400

    josh_island, torch stuff, and stuff

--- entity/player.gd
+++ entity/player.gd
@@ -1,43 +1,23 @@
 class_name Player extends CharacterBody2D
 
+
 @export var speed = 30.0
-@export var has_torch: bool = true
 @export var launch_speed = 200.0 
 @export var gravity = 500.0
 
 @onready var torch: Area2D = $Torch
 
-var hit_direction: Vector2
-var torch_velocity: Vector2
-var torch_flying: bool = false
-
 func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		torch.position = Vector2.ZERO
+		torch.top_level = false
+		torch.is_held = true
 
 	var input: Vector2 = Input.get_vector(&"MOVE_LEFT", &"MOVE_RIGHT", &"MOVE_UP", &"MOVE_DOWN")
 	velocity = input * speed
 
-	if not has_torch:
-		#torch.position += hit_direction * _delta * 15.0
-		torch_velocity.y += gravity * _delta  # Apply gravity to vertical velocity
-		torch.position += torch_velocity * _delta
-		if torch.position.y >= self.position.y:
-			torch.position.y = self.position.y
-			
-
 	move_and_slide()
 
 func hit (node: Node2D):
-	print("Hit by: " + str(node.global_position))
-	print("Current location: " + str(self.global_position))
-	has_torch = false
-
-	# Hmm somehow have to move the torch around to where it should be. 
-	torch.global_position = self.global_position
-	# torch.top_level = true
-	hit_direction = (self.global_position - node.global_position).normalized()
-	torch_velocity = hit_direction * launch_speed
-	torch_velocity.y = -launch_speed
-	
-	print("launch in direction: " + str(hit_direction.normalized()))
-	
-	
+	print("Hit by: " + str(node))
+	torch.set_moving((self.global_position - node.global_position).normalized())
---
+++ entity/torch.gd
@@ -0,0 +1,51 @@
+class_name Torch extends Area2D
+
+# TODO: should the torch follow the player mouse?
+
+
+@export var is_held: bool = true
+@export var has_hit_wall: bool = false
+@export var torch_velocity: Vector2
+
+var swing_x_axis: bool
+var swing_positivity: int
+var fly_time: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(node: Node2D):
+	if node.is_in_group("FLAMABLE"):
+		print("Node:" + str(node))
+		has_hit_wall = true
+		# node.enflame(torch)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if not is_held and not has_hit_wall:
+		fly_time += delta
+		global_position += torch_velocity * delta
+		if swing_x_axis:
+			torch_velocity.x += fly_time * swing_positivity
+		else:
+			torch_velocity.y += fly_time * swing_positivity
+
+func set_moving (direction: Vector2):
+	# if it's not held, we don't need to do anything - it's already moving
+	if not is_held: return
+	has_hit_wall = false
+	is_held = false
+	fly_time = 0.0
+
+	# set the correct global position once it becomes a top level scene
+	var _position = global_position
+	top_level = true
+	global_position = _position
+
+	# properties that make this move
+	torch_velocity = Vector2(direction)
+	torch_velocity *= 120
+	swing_x_axis = abs(torch_velocity.x) < abs(torch_velocity.y)
+	swing_positivity = (randi() & 2) - 1
