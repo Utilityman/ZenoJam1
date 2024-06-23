d584e210574a7aa3e913ed8f9657aac9e71f9a08
Josh Mackin <mackin711@gmail.com>
2024-06-23 14:52:52 -0400

    interval_spawner fixes

--- hazards/spawners/interval_projectile_spawner.gd
+++ hazards/spawners/interval_projectile_spawner.gd
@@ -2,8 +2,7 @@ class_name IntervalProjectileSpawner extends Node2D
 
 # TODO: this needs to export a whole sort of texture / animated thing (another packed scene??)
 # 	 	(the default icon.svg is unfortunately not good enough)
-@export var shape: Shape2D
-@export_range(0.1, 15.0, 0.1, "or_greater") var interval: float = 1.5
+@export_range(0.1, 15.0, 0.1, "or_greater") var interval: float = 0.0
 
 # TODO: this almost needs to take a resource that builds the emit scene instead?
 @export var projectile_data: ProjectileHazardData
@@ -12,15 +11,11 @@ class_name IntervalProjectileSpawner extends Node2D
 @onready var timer: Timer = $Timer
 
 func _ready() -> void:
-	if not collision_shape.shape:
-		collision_shape.shape = shape
-	timer.wait_time = interval
-	timer.start()
-	timer.timeout.connect(_on_timer_timeout)
+	if interval > 0:
+		timer.wait_time = interval
+		timer.start()
+		timer.timeout.connect(spawn_projectile)
 
-func _on_timer_timeout():
+func spawn_projectile():
 	var projectile: ProjectileHazard = projectile_data.create_hazard_scene()
 	add_child(projectile)
-
-
-
