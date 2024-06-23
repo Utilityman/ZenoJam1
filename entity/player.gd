e507ea6457dc5fbf59ce7b0e3cb8b66afda7c4ed
Caroline Mackin <cmackin712@gmail.com>
2024-06-23 09:46:46 -0400

    torch rises before falling

--- entity/player.gd
+++ entity/player.gd
@@ -2,10 +2,14 @@ class_name Player extends CharacterBody2D
 
 @export var speed = 30.0
 @export var has_torch: bool = true
+@export var launch_speed = 200.0 
+@export var gravity = 500.0
 
 @onready var torch: Sprite2D = $Torch
 
 var hit_direction: Vector2
+var torch_velocity: Vector2
+var torch_flying: bool = false
 
 func _physics_process(_delta: float) -> void:
 
@@ -13,7 +17,12 @@ func _physics_process(_delta: float) -> void:
 	velocity = input * speed
 
 	if not has_torch:
-		torch.position += hit_direction * _delta * 15.0
+		#torch.position += hit_direction * _delta * 15.0
+		torch_velocity.y += gravity * _delta  # Apply gravity to vertical velocity
+		torch.position += torch_velocity * _delta
+		if torch.position.y >= self.position.y:
+			torch.position.y = self.position.y
+			
 
 	move_and_slide()
 
@@ -26,5 +35,9 @@ func hit (node: Node2D):
 	torch.global_position = self.global_position
 	# torch.top_level = true
 	hit_direction = (self.global_position - node.global_position).normalized()
+	torch_velocity = hit_direction * launch_speed
+	torch_velocity.y = -launch_speed
+	
 	print("launch in direction: " + str(hit_direction.normalized()))
-
+	
+	
