e0598a967c7fbec8206946455f7f93ec5b1ae064
Caroline Mackin <cmackin712@gmail.com>
2024-06-23 23:01:17 -0400

    new sprite + character animations

--- entity/player.gd
+++ entity/player.gd
@@ -4,6 +4,7 @@ class_name Player extends CharacterBody2D
 @export var speed = 30.0
 @export var launch_speed = 200.0 
 @export var gravity = 500.0
+var last_direction: String = "IdleDown"
 
 @onready var torch: Area2D = $Torch
 
@@ -15,9 +16,58 @@ func _physics_process(_delta: float) -> void:
 
 	var input: Vector2 = Input.get_vector(&"MOVE_LEFT", &"MOVE_RIGHT", &"MOVE_UP", &"MOVE_DOWN")
 	velocity = input * speed
-
+	
+	move()
+		
+		
 	move_and_slide()
 
 func hit (node: Node2D):
 	print("Hit by: " + str(node))
 	torch.set_moving((self.global_position - node.global_position).normalized())
+
+
+func move():
+	if Input.is_action_pressed("MOVE_RIGHT"):
+		$AnimationPlayer.play("WalkRight")
+		last_direction = "Right"
+	elif Input.is_action_pressed("MOVE_LEFT"):
+		$AnimationPlayer.play("WalkLeft")
+		last_direction = "Left"
+	elif Input.is_action_pressed("MOVE_DOWN"):
+		$AnimationPlayer.play("WalkForward")
+		last_direction = "Down"
+	elif Input.is_action_pressed("MOVE_UP"):
+		$AnimationPlayer.play("WalkUp")
+		last_direction = "Up"
+	elif Input.is_action_pressed("MOVE_RIGHT") and Input.is_action_pressed("MOVE_UP"):
+		$AnimationPlayer.play("RunUpRight")
+		last_direction = "UpRight"
+	elif Input.is_action_pressed("MOVE_LEFT") and Input.is_action_pressed("MOVE_UP"):
+		$AnimationPlayer.play("RunUpLeft")
+		last_direction = "UpLeft"
+	elif Input.is_action_pressed("MOVE_RIGHT") and Input.is_action_pressed("MOVE_DOWN"):
+		$AnimationPlayer.play("RunDownRight")
+		last_direction = "DownRight"
+	elif Input.is_action_pressed("MOVE_LEFT") and Input.is_action_pressed("MOVE_DOWN"):
+		$AnimationPlayer.play("RunDownLeft")
+		last_direction = "DownLeft"
+	else:
+		match last_direction:
+			"Right":
+				$AnimationPlayer.play("IdleRight")
+			"Left":
+				$AnimationPlayer.play("IdleLeft")
+			"Down":
+				$AnimationPlayer.play("IdleDown")
+			"Up":
+				$AnimationPlayer.play("IdleUp")
+			"UpRight":
+				$AnimationPlayer.play("IdleUpRight")
+			"UpLeft":
+				$AnimationPlayer.play("IdleUpLeft")
+			"DownRight":
+				$AnimationPlayer.play("IdleDownRight")
+			"DownLeft":
+				$AnimationPlayer.play("IdleDownLeft")
+	
--- hazards/objects/projectile_hazard_data.gd
+++ hazards/objects/projectile_hazard_data.gd
@@ -12,13 +12,13 @@ var projectile_packed: PackedScene = preload("res://hazards/objects/projectile_h
 # TODO: needs visual information to be able to pass into the hazard
 
 func create_hazard_scene () -> ProjectileHazard:
-    var projectile_scene: ProjectileHazard = projectile_packed.instantiate()
-    projectile_scene.shape = shape
-    projectile_scene.lifetime = lifetime
-    projectile_scene.direction = direction
-    projectile_scene.speed = speed
-    var visuals = visual_scene.instantiate()
-    projectile_scene.add_child(visuals)
+	var projectile_scene: ProjectileHazard = projectile_packed.instantiate()
+	projectile_scene.shape = shape
+	projectile_scene.lifetime = lifetime
+	projectile_scene.direction = direction
+	projectile_scene.speed = speed
+	var visuals = visual_scene.instantiate()
+	projectile_scene.add_child(visuals)
 
-    return projectile_scene
+	return projectile_scene
 
